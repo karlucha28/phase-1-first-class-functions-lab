daa117eaaeb9f550851a9b93789fc7f6a0b6aba3
KarlSavinskiy <karlsavinskiy@gmail.com>
2022-07-21 02:51:25 -0400

    commit tree 283f45cab0cc4492c848ce1f825ce3589fc9f336

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(oneArgument){ 
+    return oneArgument.slice(0, 2);
+}
+const returnLastTwoDrivers = function(secondArgument) {
+    return secondArgument.slice(-2)
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+const createFareMultiplier = function(a) {
+    return function(b) {
+        return a*b
+    }
+
+}
+
+    function selectDifferentDrivers(oneArgument, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(oneArgument);
+ }
+
+
+
+
+
+
+    //fanction takes in an integer, multiplier
+    //returns a anonymous function
+    //function tskes parametr fare 
+    //multiply fare by multiplier
+ const fareDoubler = createFareMultiplier(2) 
+
+ fareDoubler() // to run it
+
+ const fareTripler = createFareMultiplier(3)
+
+ fareTripler() 
